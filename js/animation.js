925432d9827ef940a3157bb0f8503e7721cf9d63
ALOTIN Beh Ardieu <alotinbehardieu@gmail.com>
2022-11-27 18:09:45 +0100

    first commit

---
+++ js/animation.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.logo', { duration: 2000, delay: 2000 });
+ScrollReveal().reveal('.content', { duration: 2000, delay: 2500 });
+ScrollReveal().reveal('.widget', { interval: 400 });
+
+
